c5006321984b3d74214f9fb0436973035135e21d
BT <bahartofigh@gmail.com>
2024-08-09 01:29:54 +0330

    develop blog cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="bg-purple-100">
           <div className="container mx-auto lg:max-w-screen-xl">
-          <div className='grid gap-4 md:grid-cols-12 md:grid-rows-[60px_minmax(300px,_1fr)] min-h-screen'>
+          <div className='grid gap-8 md:grid-cols-12 md:grid-rows-[60px_minmax(300px,_1fr)] min-h-screen'>
       <div className='hidden md:block md:row-span-2 md:col-span-3'>
         <div className="bg-white rounded-3xl overflow-hidden">
           {/* سربرگ منوی آکاردئونی */}
@@ -50,8 +50,29 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div className='bg-blue-300 md:col-span-9'>Blogs</div>
-    </div>
+      {/* بخش بلاگ */}
+      <div className='grid grid-cols-6 gap-4 md:col-span-9'>{["2.png","3.png","4.png","vuejs.png","nuxt-icon.png", "next-JS-framework.png"].map((item, index) => {
+        return (
+          <div className="bg-white col-span-6 md:col-span-3 lg:col-span-2 rounded-3xl p-2 object-cover w-full h-full object-center" key={index}>
+          {/* تصویر مقاله */}
+          <div className="aspect-h-9 aspect-w-16">
+            <img src={`/images/${item}`} alt="" className="rounded-2xl" />
+          </div>
+          {/* محتوا */}
+          <div className="bg-gray-50 py-2 rounded-2xl">
+            <h2 className="mb-4 text-bold">بررسی کامل ریکت و ریداکس</h2>
+            <div className='flex items-center justify-between'>
+              <div className="flex items-center">
+                <img src="/images/nodejs-logo.png" alt="" className="rounded-full w-6 h-6 ring-2 ring-violet-600 ml-2" />
+                <span>بهار توفیق</span>
+              </div>
+              <span className="text-xs px-2 py-1 bg-purple-300 text-purple-600 rounded-xl hover:bg-purple-600 hover:text-purple-300 transition-all duration-300 cursor-pointer">ری‌اکت</span>
+            </div>
+          </div>
+        </div>
+        )
+      })}</div>
+          </div>
     </div>
     </div>
   )
