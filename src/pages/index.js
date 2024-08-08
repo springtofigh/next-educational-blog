346a30ac378299d4bf892d41ce21a18ab131c894
BT <bahartofigh@gmail.com>
2024-08-08 23:14:01 +0330

    add container class for fixing width

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,58 @@
-import styles from '../../styles/Home.module.css';
 import { ChevronDownIcon, AdjustmentsIcon, HeartIcon, BookmarkIcon, AnnotationIcon, ClockIcon } from "@heroicons/react/outline";
 import Link from 'next/link';
 import { useState } from 'react';
 
 
 export default function Home() {
-  
+  const [isOpen, setIsOpen] = useState(false)
   return (
-<div className='gird gap-4 md:grid-cols-12 md:grid-rows-2'>
-  <div className='bg-blue-300 hidden md:block md:col-span-3'>Desktop Categories</div>
-  <div className='bg-blue-300 hidden md:block'>Desktop Sort</div>
-  <div className='bg-blue-300 md:col-span-9'>Blogs</div>
-</div>
+    <div className="bg-purple-100">
+          <div className="container mx-auto lg:max-w-screen-xl">
+          <div className='grid gap-4 md:grid-cols-12 md:grid-rows-[60px_minmax(300px,_1fr)] min-h-screen'>
+      <div className='hidden md:block md:row-span-2 md:col-span-3'>
+        <div className="bg-white rounded-3xl overflow-hidden">
+          {/* سربرگ منوی آکاردئونی */}
+        <div className="flex items-center justify-between px-4 py-4 cursor-pointer bg-purple-200" onClick={() => setIsOpen(!isOpen)}>
+          <span>دسته‌بندی مقالات</span>
+          <ChevronDownIcon className={`w-6 h-6 stroke-purple-400 transition-all duration-200 ${isOpen ? 'rotate-180' : 'rotate-0'}`}/>
+        </div>
+        {/* محتوای منوی آکاردئونی */}
+        <div className={`${isOpen ? "block" :"hidden"}`}>
+          <Link href="#">
+          <span className="block pr-4 py-2 hover:bg-purple-500 mb-1">
+            همه مقالات
+          </span>
+          </Link>
+          <Link href="#">
+          <span className="block pr-4 py-2 hover:bg-purple-500 mb-1">
+            ریکت
+          </span>
+          </Link>
+          <Link href="#">
+          <span className="block pr-4 py-2 hover:bg-purple-500">
+            جاوااسکریپت
+          </span>
+          </Link>
+        </div>
+        
+        </div>
+      </div>
+      <div className='hidden md:block md:col-span-9'>
+        <div className="bg-slate-100 rounded-xl px-4 flex items-center">
+          <div className="flex items-center gap-x-2 ml-2">
+            <AdjustmentsIcon className='w-6 h-6'/>
+            <span className='text-gray-700'>مرتب سازی:</span>
+          </div>
+          <ul className="flex items-center gap-x-4">
+            <li className='cursor-pointer text-gray-700 py-4'>پر بازدیدترین</li>
+            <li className='cursor-pointer text-gray-700 py-4'>محبوب ترین</li>
+            <li className='cursor-pointer text-gray-700 py-4'>جدید ترین</li>
+          </ul>
+        </div>
+      </div>
+      <div className='bg-blue-300 md:col-span-9'>Blogs</div>
+    </div>
+    </div>
+    </div>
   )
 }
