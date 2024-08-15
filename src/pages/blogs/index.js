c4f654a4d6797a6098f27bee2ec8ca284ac02344
BT <bahartofigh@gmail.com>
2024-08-15 17:42:05 +0330

    add desktop and mobile category and sort bar components

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import PostList from "@/components/Posts/PostList";
+import MobileCategory from "@/components/Posts/MobileCategory";
+import SortBar from "@/components/Posts/SortBar";
+import DesktopCategory from "@/components/Posts/DesktopCategory";
 
-function Blog() {
+
+function Blog({ blogsData, postCategories }) {
   return (
-    <div>Blog</div>
+    <div className="bg-purple-100">
+    <div className="container mx-auto lg:max-w-screen-xl px-4 md:px-0">
+            {/* Mobile category */}
+            <MobileCategory postCategories={ postCategories } />
+        <div className='grid gap-8 md:grid-cols-12 md:grid-rows-[60px_minmax(300px,_1fr)] min-h-screen'>
+            <div className='hidden md:block md:row-span-2 md:col-span-3'>
+            <DesktopCategory postCategories={ postCategories } />
+            </div>
+            {/* Desktop sortbar */}
+            <div className='hidden md:block md:col-span-9'>
+              <SortBar />
+            </div>
+            {/* Blogs Section */}
+            <div className='grid grid-cols-6 gap-4 md:col-span-9'>
+              <PostList  blogsData = { blogsData } />
+            </div>
+        </div>
+    </div>
+  </div>
   )
 }
 
-export default Blog
+export default Blog;
+
+
+export async function getServerSideProps(context) {
+  const { data: result } = await axios.get('http://localhost:5000/api/posts?page=1&limit=6');
+  const { data: postCategories } = await axios.get('http://localhost:5000/api/post-category')
+  const { data } = result;
+
+  return { props: { blogsData: data, postCategories: postCategories.data } }
+}
