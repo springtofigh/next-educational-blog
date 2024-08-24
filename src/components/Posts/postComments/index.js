eb29917c1944b84d285d6691e15dd476e4f00a2e
BT <bahartofigh@gmail.com>
2024-08-24 21:01:31 +0330

    nested comment development

--- src/components/Posts/postComments/index.js
+++ src/components/Posts/postComments/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SingleComment from "./SingleComment";
 import CommentForm from "./commentForm";
+import ReplyComment from "./ReplyComment";
 
 function PostComments({ post }) {
   const [commentValue, setCommentValue] = useState(" ");
@@ -8,11 +9,14 @@ function PostComments({ post }) {
     <div>
         <h2 className="font-black text-2xl mb-8">نظرات</h2>
         {post.comments.map((comment) => {
-                return !comment.responseTo && comment.status === 2 && <React.Fragment key={comment._id}>
+                return !comment.responseTo && comment.status === 2 && (
+                  <React.Fragment key={comment._id}>
                   <SingleComment comment={comment} />
+                  <ReplyComment comments={post.comments} parentCommentId={comment._id} />
                 </React.Fragment>
-            })}
-
+                )
+            })
+          }
             {/* Send Comments */}
             <div className="mt-8">
                 <span className="font-bold md:text-lg">ارسال نظر جدید</span>
