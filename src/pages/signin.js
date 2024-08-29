8aaaeb9039820c2c5db08806898875cefd2caf5a
BT <bahartofigh@gmail.com>
2024-08-29 22:32:13 +0330

    signin page create

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,20 +1,52 @@
-import { Formik } from 'formik';
-import * as yup from 'yup';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import InputComponent from '@/components/FormInput';
 import Link from 'next/link';
 import Layout from '@/containers/Layout';
 import Head from 'next/head';
 
+// Initial values
+const initialValues = {
+    email: '',
+    password: ''
+}
+
+// Validation schema
+const validationSchema = Yup.object({
+    email: Yup.string().email('ایمیل نامعتبر است').required('لطفا ایمیل را وارد کنید'),
+    password: Yup.string()
+    .required("لطفا رمز عبور را وارد نمائید")
+    .min(8, "پسورد شما خیلی کوتاه است - رمز عبور باید حداقل ۸ کرکتر باشد"),
+})
+
+
+
 function RegisterForm() {
+    // onSubmit
+    const onSubmit = (values) => {
+        const { email, password } = values
+    }
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit,
+        validationOnMount: true
+    });
+
   return (
     <Layout>
         <Head>
             <title></title>
         </Head>
         <div className='container mx-auto md:max-w-md px-4 md:px-4'>
-            <form className='flex flex-col'>
+            <form onSubmit={formik.handleSubmit} className='flex flex-col space-y-4'>
                 <h2 className='font-bold text-2xl text-violet-700 mb-4'>ورود</h2>
-
+                <InputComponent label='ایمیل' name='email' formik={formik} />
+                <InputComponent label='رمز عبور' name='password' type='password' formik={formik} />
                 <button
+                type='submit'
+                disabled={!formik.isValid}
                 className='w-full py-2 bg-violet-800 text-white rounded-lg'
                 >
                     ورود
