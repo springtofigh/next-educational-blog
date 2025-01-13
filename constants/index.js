43cf340e06cc934a0870c79b9564172b8dda3145
BT <bahartofigh@gmail.com>
2025-01-14 01:41:21 +0330

    static datas

---
+++ constants/index.js
@@ -0,0 +1,107 @@
+export const heroSocials = [
+    {
+      name: 'reddit',
+      url: '/images/reddit.svg',
+    },
+    {
+      name: 'discord',
+      url: '/images/discord.svg',
+    },
+    {
+      name: 'twitter',
+      url: '/images/twitter.svg',
+    },
+    {
+      name: 'instagram',
+      url: '/images/instagram.svg',
+    },
+  ];
+  
+  export const exploreWorlds = [
+    {
+      id: 'جهان پنجم',
+      imgUrl: '/images/planet-05.png',
+      title: 'آزمایشگاه هاوکینز',
+    },
+    {
+      id: 'جهان چهارم',
+      imgUrl: '/images/planet-04.png',
+      title: 'جزیره بهشت',
+    },
+    {
+      id: 'جهان سوم',
+      imgUrl: '/images/planet-03.png',
+      title: 'هرم کادیروجو',
+    },
+    {
+      id: 'جهان دوم',
+      imgUrl: '/images/planet-02.png',
+      title: 'دنیای وارونه',
+    },
+    {
+      id: 'جهان اول',
+      imgUrl: '/images/planet-01.png',
+      title: 'هاگوارتز',
+    },
+  ];
+  
+  export const startingFeatures = [
+    'دنیایی پیدا کن که مناسبته و میخواهی به آن وارد بشوی',
+    'با گفتن نام و یاد خدا وارد شوید تا در امان بمانید',
+    'نیازی به درگیری در اطراف جهان ها نیست، فقط روی جو بمانید و لذت ببرید',
+  ];
+  
+  export const newFeatures = [
+    {
+      imgUrl: '/images/vrpano.svg',
+      title: 'یک دنیای تازه',
+      subtitle:
+          'ما آخرین به روز رسانی با دنیای جدید را برای شما داریم با خیال راحت امتحان کنید',
+    },
+    {
+      imgUrl: '/images/headset.svg',
+      title: 'واقعی تر',
+      subtitle:
+          'در آخرین به روز رسانی، چشمان شما باز است و دنیا را واقعی تر از همیشه میبیند',
+    },
+  ];
+  
+  export const insights = [
+    {
+      imgUrl: '/images/planet-06.png',
+      title: 'راه اندازی متاورس باعث می شود ایلان ماسک علاقه مند تر شود',
+      subtitle:
+          'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است',
+    },
+    {
+      imgUrl: '/images/planet-07.png',
+      title: '۷ نکته برای تسلط آسان بر جنون متاورس',
+      subtitle:
+          'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    },
+    {
+      imgUrl: '/images/planet-08.png',
+      title: 'با یک پلتفرم می توانید تمام دنیا را به صورت مجازی کشف کنید',
+      subtitle:
+          'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    },
+  ];
+  
+  export const helps = [
+    {
+      name: 'تضمین کاملترین محتوا',
+      url: '/images/',
+    },
+    {
+      name: '',
+      url: '/images/',
+    },
+    {
+      name: '',
+      url: '/images/g',
+    },
+    {
+      name: '',
+      url: '/images/',
+    },
+  ];
