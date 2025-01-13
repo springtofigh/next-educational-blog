dcd6b9844f30eac3f7d0a04d635e9d90c54cbfd3
BT <bahartofigh@gmail.com>
2025-01-13 23:46:58 +0330

    add new properties in theme and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,19 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/common/**/*.{js,ts,jsx,tsx}",
     "./src/containers/**/*.{js,ts,jsx,tsx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["shabnam"],
+      },
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
       },
     }
   },
