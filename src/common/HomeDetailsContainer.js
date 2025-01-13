01358818bffb777cd949a33ec952f40687103e29
BT <bahartofigh@gmail.com>
2025-01-14 01:32:47 +0330

    add component for blogs category in home

--- src/common/HomeDetailsContainer.js
+++ src/common/HomeDetailsContainer.js
@@ -1,10 +1,16 @@
-import HomePagePosts from '@/components/Posts/HomePagePosts';
+import HomePagePosts from '@/components/Posts/HomePage/HomePagePosts';
+import HelpSection from '@/components/HomeSections/HelpSection';
+import PopularPosts from '@/components/Posts/HomePage/PopularPosts';
+import CategoryPosts from '@/components/Posts/HomePage/CategoryPosts';
 
-function HomeDetailsContainer({ blogsData }) {
+function HomeDetailsContainer({ blogsData, postCategories }) {
     console.log(blogsData);
   return (
     <main className='max-w-[1920px] mx-auto overflow-x-hidden'>
-        <HomePagePosts blogsData={blogsData}/>
+        <HomePagePosts blogsData={blogsData} />
+        <CategoryPosts postCategories={postCategories} />
+        <PopularPosts postCategories={postCategories} />
+        <HelpSection />
     </main>
   )
 }
