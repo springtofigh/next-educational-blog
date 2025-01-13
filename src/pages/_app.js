0d4c1dddf75889ab0fcd0424dbbba0f46d85c251
BT <bahartofigh@gmail.com>
2025-01-13 23:56:14 +0330

    Head and title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import '../../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import AuthProvider from '@/context/AuthContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+    <Head>
+        <title>
+        فرانت تاپ لرن | دنیای کدهای بی‌نهایت
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </Head>
       <Component {...pageProps} />
       <Toaster position="top-right" />
     </AuthProvider>
